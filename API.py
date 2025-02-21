0594fe53adc98bf0cef971eb330d2baf65f09f55
binlada25@gmail.com <binlada25@gmail.com>
2025-02-20 22:00:41 -0300

    Revert "All update"
    This reverts commit 3722bc60c3ee39d81501f94a424bc29dd950f71a.

--- API.py
+++ API.py
@@ -1,54 +1,20 @@
 from flask import Flask, request, jsonify
-import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
 import mysql.connector
 
 app = Flask(__name__)
 
+# Conexão com o bd (banco de dados)
 def conectar_bd():
     return mysql.connector.connect(
         host="localhost",
         user="root",
         password="71208794",
-        database="base_email_marketing"
+        database="email_marketing"
     )
 
 @app.route('/')
 def home():
     return jsonify({"mensagem": "API funcionando! Use as rotas disponíveis."})
-
-@app.route('/enviar_email', methods=['POST'])
-def enviar_email():
-    dados = request.json
-    destinatarios = dados.get('destinatarios')  # Lista de emails
-    assunto = dados.get('assunto')
-    mensagem = dados.get('mensagem')
-
-    try:
-        # Configurações do servidor SMTP (Gmail)
-        remetente = "seu_email@gmail.com"
-        senha = "sua_senha"
-
-        for destinatario in destinatarios:
-            # Criação do email
-            msg = MIMEMultipart()
-            msg['From'] = remetente
-            msg['To'] = destinatario
-            msg['Subject'] = assunto
-            msg.attach(MIMEText(mensagem, 'plain'))
-
-            # Envio de email
-            server = smtplib.SMTP('smtp.gmail.com', 587)
-            server.starttls()
-            server.login(remetente, senha)
-            server.sendmail(remetente, destinatario, msg.as_string())
-            server.quit()
-
-        return jsonify({"mensagem": "Emails enviados com sucesso!"})
-    except Exception as e:
-        return jsonify({"erro": str(e)}), 500
-    
 
 # Adicionar email (evita duplicados)
 @app.route('/adicionar_email', methods=['POST'])
