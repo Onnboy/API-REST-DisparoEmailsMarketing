ba974196c1db9c4b68ddef1a215ce7544c44f309
André Messias <binlada25@gmail.com>
2025-02-25 11:40:46 -0300

    Delete testes directory

--- testes/test_env.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-print("DB_USER:", os.getenv("DB_USER"))
-print("DB_PASSWORD:", os.getenv("DB_PASSWORD"))
-print("DB_NAME:", os.getenv("DB_NAME"))
